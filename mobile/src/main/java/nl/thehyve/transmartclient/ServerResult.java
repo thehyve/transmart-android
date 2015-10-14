99ac101a6a670705387ba100b0b4ce0253113354
Ward Weistra <ward@thehyve.nl>
2015-10-14 10:43:00 +0200

    Made ServerResult Parcellable

--- mobile/src/main/java/nl/thehyve/transmartclient/ServerResult.java
+++ mobile/src/main/java/nl/thehyve/transmartclient/ServerResult.java
@@ -1,5 +1,9 @@
 package nl.thehyve.transmartclient;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.util.Log;
+
 /**
  * Created by Ward Weistra on 22-12-14.
  * Copyright (c) 2015 The Hyve B.V.
@@ -7,7 +11,10 @@ package nl.thehyve.transmartclient;
  * version 3, or (at your option) any later version.
  */
 
-public class ServerResult {
+public class ServerResult implements Parcelable {
+
+    private static final String TAG = "ServerResult";
+
     private int responseCode;
     private String responseDescription;
     private String result;
@@ -23,6 +30,23 @@ public class ServerResult {
         this.responseDescription = responseDescription;
         this.result = result;
     }
+
+    public ServerResult(Parcel in) {
+        readFromParcel(in);
+        Log.d(TAG, "ServerResult has been instantiated from parcel.");
+    }
+
+    public static final Creator<ServerResult> CREATOR = new Creator<ServerResult>() {
+        @Override
+        public ServerResult createFromParcel(Parcel in) {
+            return new ServerResult(in);
+        }
+
+        @Override
+        public ServerResult[] newArray(int size) {
+            return new ServerResult[size];
+        }
+    };
 
     public int getResponseCode() {
         return responseCode;
@@ -46,5 +70,29 @@ public class ServerResult {
 
     public void setResult(String result) {
         this.result = result;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    // Method to write the data to a Parcel
+    @Override
+    public void writeToParcel(Parcel out, int flags) {
+        out.writeInt(responseCode);
+        out.writeString(responseDescription);
+        out.writeString(result);
+
+        Log.d(TAG, "ServerResult has been written to parcel.");
+    }
+
+    // Method to read the data from a Parcel
+    private void readFromParcel(Parcel in) {
+        this.responseCode = in.readInt();
+        this.responseDescription = in.readString();
+        this.result = in.readString();
+
+        Log.d(TAG, "ServerResult has been read from parcel.");
     }
 }
